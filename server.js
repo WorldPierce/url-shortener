f0ea0e54f24bfc816bcc13e98cfd69fd347deee5
System <none>
2017-07-07 17:08:44 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var connected=false;
 
 //connect to database mongoose pluralizes connections
 var MONGODB_URI = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'https://deep-level.glitch.me/models/shortUrls.js');
 
 var datastore = require('./datastore').sync;
